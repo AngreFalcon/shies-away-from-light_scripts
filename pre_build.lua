0a81a0609737bc88bf2a591b02339c07e386993d
Sorre <angrepenguin@gmail.com>
2025-11-18 00:57:24 -0500

    changed back slashes to forward slashes in paths for compatibility

--- pre_build.lua
+++ pre_build.lua
@@ -1,14 +1,14 @@
 local lfs = require"lfs"
 
 local projDir = lfs.currentdir()
-local src = projDir .. "\\scripts\\scripts\\safl_shies\\common.tl"
-local dest = projDir .. "\\scripts\\safl_shies"
+local src = projDir .. "/scripts/scripts/safl_shies/common.tl"
+local dest = projDir .. "/scripts/safl_shies"
 
 for file in lfs.dir(dest) do
 	if file ~= nil and file ~= "." and file ~= ".." then
 		if string.sub(file, -4) == ".lua" then
-			print(dest .. "\\" .. file)
-			os.remove(dest .. "\\" .. file)
+			print(dest .. "/" .. file)
+			os.remove(dest .. "/" .. file)
 		end
 	end
 end
@@ -18,7 +18,7 @@ if infile ~= nil then
 	local instr = infile:read("*a")
 	infile:close()
 
-	local outfile = io.open(dest .. "\\common.tl", "w")
+	local outfile = io.open(dest .. "/common.tl", "w")
 	outfile:write(instr)
 	outfile:close()
 end
