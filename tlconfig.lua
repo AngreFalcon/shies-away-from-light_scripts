15044d86fc021e309aca11f6829b68d80e61d2e7
Sorre <angrepenguin@gmail.com>
2025-11-17 19:14:56 -0500

    cleaned up tlconfig

--- tlconfig.lua
+++ tlconfig.lua
@@ -2,12 +2,10 @@ return {
     source_dir = 'scripts',
     build_dir = 'scripts',
     exclude = {
-        "**/*.lua"
+        '**/*.lua',
     },
     include_dir = {
 		'include/openmw',
-		'scripts/safl_shies',
-        ''
 	},
     gen_target = '5.1',
 }
