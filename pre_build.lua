8f087ddc4ff04f5cf55048865529ba8b4804c18a
Sorre <angrepenguin@gmail.com>
2025-11-24 21:38:43 -0500

    moved most contents of shies.tl into follower.tl in an attempt to prepare follower scripts for inheritance

--- pre_build.lua
+++ pre_build.lua
@@ -1,10 +1,11 @@
 local lfs = require"lfs"
 
 local projDir = lfs.currentdir()
-local src = projDir .. "/scripts/scripts/follower/common.tl"
+local src = projDir .. "/scripts/scripts/follower"
 local dest = projDir .. "/scripts/follower"
+local fileNames = {}
 
-for file in lfs.dir(dest) do
+for file in lfs.dir(dest) do -- Deletes all .lua files in build directory
 	if file ~= nil and file ~= "." and file ~= ".." then
 		if string.sub(file, -4) == ".lua" then
 			os.remove(dest .. "/" .. file)
@@ -12,13 +13,23 @@ for file in lfs.dir(dest) do
 	end
 end
 
-local infile = io.open(src, "r")
-if infile ~= nil then
-	local instr = infile:read("*a")
-	infile:close()
-	local outfile = io.open(dest .. "/common.tl", "w")
-	if outfile ~= nil then
-		outfile:write(instr)
-		outfile:close()
+for file in lfs.dir(src) do
+	if file ~= nil and file ~= "." and file ~= ".." then
+		if string.sub(file, -3) == ".tl" then
+			table.insert(fileNames, file)
+		end
+	end
+end
+for _, fileName in ipairs(fileNames) do
+	local infile = io.open(src .. "/" .. fileName, "r")
+	if infile ~= nil then
+		local instr = infile:read("*a")
+		infile:close()
+
+		local outfile = io.open(dest .. "/" .. fileName, "w")
+		if outfile ~= nil then
+			outfile:write(instr)
+			outfile:close()
+		end
 	end
 end
